9a161eb3c54a0bb57871a9326657c69cd78460a8
满浩龙 <manhaolong@126.com>
2018-03-21 00:16:45 +0800

    JVM抓内存快照

---
+++ src/main/java/JVM/JVMTest.java
@@ -0,0 +1,25 @@
+package JVM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JVMTest {
+
+	byte[] byteArray = new byte[1*1024*1024];//1MB
+	public static void main(String[] args) {
+		List<JVMTest> list = new ArrayList<JVMTest>();
+		int count = 0;
+		try {
+			while(true) {
+				
+				list.add(new JVMTest());
+				count = count + 1;
+			}
+		} catch (Throwable e) {
+			System.out.println("*******count:"+count);
+			e.printStackTrace();
+		}
+		
+	}
+
+}
